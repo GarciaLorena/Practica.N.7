0f221bf00fb8f76162cf82339c7c1f300aff6727
GarciaLorena <32082577+GarciaLorena@users.noreply.github.com>
2017-10-16 19:26:31 -0500

    Agregar archivos de proyecto.

---
+++ App5/App5.UWP/SQLiteUWP.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using App5.UWP;
+using Xamarin.Forms;
+using Windows.Storage;
+
+
+[assembly: Dependency(typeof(SQLiteUWP))]
+
+namespace App5.UWP
+{
+   public class SQLiteUWP : ISQLite
+    {
+        public string GetLocalFilePath(string filename)
+        {
+            return Path.Combine(ApplicationData.Current.LocalFolder.Path, filename);
+        }
+    }
+}
---
+++ App5/App5/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using SQLite;
+
+namespace App5
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            SQLiteAsyncConnection database;
+            string db;
+
+            db = DependencyService.Get<ISQLite>().GetLocalFilePath("TESHDB.db");
+            database = new SQLiteAsyncConnection(db);
+            database.CreateTableAsync<TESHDatos>().Wait();
+
+            var elemento = new TESHDatos {
+            Id = "13090351",
+            Dato1 = "Lorena",
+            Dato2 = "GarciaHernandez",
+            Dato3 = "Sana Francisco",
+            Dato4 = "5548566929",
+            Dato5 = "ISC",
+            Dato6 = "Noveno",
+            Dato7 = "lorenatesh@hotmail.com",
+            Dato8 = "GarciaLorena"
+        };
+            database.InsertAsync(elemento);
+
+
+        }
+    }
+}
